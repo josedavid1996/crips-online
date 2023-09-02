b464c444a97cd34fd30412c4c66c61add1db0aa7
jose david <81387297+josedavid1996@users.noreply.github.com>
2023-09-02 15:27:05 -0500

    Settings de imagenkit

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,12 @@
 // Put your application javascript here
+const url = document.querySelector(".url")
+console.log(url)
+
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(() => {
+
+    document.querySelector(".url").style.display = "block";
+    document.querySelector(".url").style.color = "red";
+  }, 2000);
+  // Tu código para mostrar el elemento aquí
+});
